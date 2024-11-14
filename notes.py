c088f05bf0b370d6b978665ab4b6b18a9a3a64a7
https://github.com/renatomorenoagudo <renatomorenoagudo2@gmail.com>
2024-11-13 23:21:52 -0300

    structures basics main

---
+++ notes.py
@@ -0,0 +1,35 @@
+#!usr/Scriptenv python
+
+""""BLOCO DE NOTAS
+
+$notes.py new "Minha Nota"
+tag: tech
+text:
+Anotação geral sobre carreira de tecnologia
+
+$ notes.py read --tag=tech
+'''
+'''
+"""
+__version__ = "0.1.0"
+
+import os
+import sys
+
+path = os.curdir
+filepath = os.path.join(path, "notes.txt")
+
+arguments = sys.argv[1:]
+if not arguments:
+    print("Invalid usage")
+    sys.exit(1)
+
+cmds = ("read", "new")
+if arguments[0] not in cmds:
+    print("Invalid command {argument[0]}")
+
+if arguments[0] == "read":
+    #leitura das notas
+
+if arguments[0] == "new":
+    #salvar
