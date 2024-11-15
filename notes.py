d5a13e7f2c2770595ae820f0d16c4fbab522036f
https://github.com/renatomorenoagudo <renatomorenoagudo2@gmail.com>
2024-11-14 21:37:32 -0300

    programa rodando e testado

--- notes.py
+++ notes.py
@@ -7,7 +7,7 @@ tag: tech
 text:
 Anotação geral sobre carreira de tecnologia
 
-$ notes.py read --tag=tech
+$ notes.py read tech
 '''
 '''
 """
@@ -15,22 +15,34 @@ __version__ = "0.1.0"
 
 import os
 import sys
-
+cmds = ("read", "new")
 path = os.curdir
 filepath = os.path.join(path, "notes.txt")
 
 arguments = sys.argv[1:]
 if not arguments:
-    print("Invalid usage")
+    print("Invalid usage"),
+    print(f"you mus specify subcomand")
     sys.exit(1)
 
-cmds = ("read", "new")
+
 if arguments[0] not in cmds:
     print("Invalid command {argument[0]}")
 
 if arguments[0] == "read":
     #leitura das notas
+    pass
+    #for line open(filepath):
+      #  title, tag, text = line.split("\t")
+       # if tag.lower()  == 
 
 if arguments[0] == "new":
     titulo = arguments [1]  #TODO: tratar exception
-    #criação da nota
+    text = [
+        f"{titulo}",
+        input ("tag:").strip(), 
+        input ("text:\n").strip(),
+    ]
+    # \t - tsv
+    with open (filepath, "a") as file_:
+        file_.write("\t".join(text))
